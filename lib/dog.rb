02d2139caefe3ffae7ff28636c9a24c09c68b311
Mohammad S Hussain <m.s.hussain@outlook.com>
2020-08-28 20:31:29 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,12 @@
+class Dog
+  
+def fido
+ fido = Dog.new
+  
+def snoop
+  snoop = Dog.new
+  
+def lassie
+  lassie = Dog.new
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,8 @@
+class Person
+  
+def adele_goldberg = Person.new
+  
+def alan_kay = Person.new
+  
+end 
+  
